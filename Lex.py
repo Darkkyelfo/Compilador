f91e6be9e7b4928addd3ec167124bf876c52635f
Darkkyelfo <raulpedrouag@gmail.com>
2016-09-02 00:32:35 -0300

    Analisador léxico agora avisa sobre a existência de tokens inválidos.

--- Lex.py
+++ Lex.py
@@ -7,6 +7,7 @@ Created on 30 de ago de 2016
 import re
 from TabelaSimbolos import TabelaDeSimbolos
 from Lexema import *
+from TolkenInvalidoError import *
 
 class Lex(object):
     listaTolkens = []
@@ -31,14 +32,26 @@ class Lex(object):
                             posicao = match.start()#pega a posicao na linha do lexema encontrado
                             lexema = Lexema(match.group(),tolken,num+1,posicao)#cria o lexema
                             listaLex.append(lexema)#add os lexemas a lista
+                            linha = linha.replace(lexema.lexema," ")#Apaga o lexema encontrado da string
+                    self.__houveTokenInvalido(linha,num)#detecta se na linha houve algum tolken ilegal
                     listaLex.sort(key=lambda x: x.posicao, reverse=False)#Organiza os tolkens na ordem que aparecem na lista 
                     for i in listaLex:#add os lexemas encontrados a tabela de símbolos
                         self.tSimbolos.addSimbolo(i)
             except(TypeError):
                 print(u"Sua expressão regular não é precisa o suficiente")
-            
+            except (TolkenInvalidoError) as e:
+                print(str(e))
             return self.tSimbolos
     
+    #Função responsavel por detectar se existe algum token inválido(não definido)
+    #na string. Para isso ela verifica se a linha é composta somente por " "
+    #pois,sempre que umlexema é encontrado no pelo lex ele o remove da string. 
+    def __houveTokenInvalido(self,linha,num):
+        for i in linha:
+            if(i!=" "):
+                raise TolkenInvalidoError(u"Tolken Inválido:%s na linha:%s"%(i,num+1))
+                break
+            
             
         
         
---
+++ TolkenInvalidoError.py
@@ -0,0 +1,13 @@
+'''
+Created on 1 de set de 2016
+
+@author: Raul
+'''
+#-*-encoding:utf-8-*-
+class TolkenInvalidoError(Exception):
+    u'''
+    classe para lançar a exce��o de token inv�lido
+    '''
+    pass
+
+        
